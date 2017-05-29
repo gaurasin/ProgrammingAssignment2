e7166111f2f395e6ff9fb786456c4258d257114a
Gaurav <gauravsinghal.ieta@gmail.com>
2017-05-29 16:09:44 +0800

    adding Inverse File with Comment updated

--- cachematrix.R
+++ cachematrix.R
@@ -24,7 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## The following function calculates the Inverse of the special "Matrix" created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
+## The following function calculates the Inverse of the special "Matrix" 
+##created with the above function. However, it first checks to see if the 
+##inverse has already been calculated. If so, it gets the inverse from 
+##the cache and skips the computation. Otherwise, it calculates the inverse of the 
+##data and sets the value of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
